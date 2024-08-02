34746fba426ff4a0e7088d72d0ac980e5f3fcbd9
adeeshajayasinghe <jayasingheai.20@uom.lk>
2024-08-02 16:46:09 +0530

    Added error handling in embedding functionality

--- utils/embed_data.py
+++ utils/embed_data.py
@@ -6,41 +6,45 @@ from langchain_community.vectorstores import FAISS
 
 
 async def embed_docs():
-    # Get the directory containing the executable (assuming a single-file executable)
-    script_dir = os.path.dirname(os.path.realpath(__file__))
+    try:
+        # Get the directory containing the executable (assuming a single-file executable)
+        script_dir = os.path.dirname(os.path.realpath(__file__))
 
-    # Define a subdirectory within the executable's directory to store docs
-    data_dir = os.path.join(script_dir, "data")
+        # Define a subdirectory within the executable's directory to store docs
+        data_dir = os.path.join(script_dir, "data")
 
-    # Create the directory if it doesn't exist
-    os.makedirs(data_dir, exist_ok=True) 
+        # Create the directory if it doesn't exist
+        os.makedirs(data_dir, exist_ok=True) 
 
-    # Load pdf files in the local directory
-    loader = PyPDFDirectoryLoader(data_dir)
-    docs_before_split = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 700,
-        chunk_overlap  = 50,
-    )
-    docs_after_split = text_splitter.split_documents(docs_before_split)
-    huggingface_embeddings = HuggingFaceBgeEmbeddings(
-        model_name="BAAI/bge-small-en-v1.5", 
-        model_kwargs={'device':'cpu'}, 
-        encode_kwargs={'normalize_embeddings': True}
-    )
-    # Define a subdirectory within the executable's directory to store FAISS data
-    vector_db_dir = os.path.join(script_dir, "vector_db")
+        # Load pdf files in the local directory
+        loader = PyPDFDirectoryLoader(data_dir)
+        docs_before_split = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 700,
+            chunk_overlap  = 50,
+        )
+        docs_after_split = text_splitter.split_documents(docs_before_split)
+        huggingface_embeddings = HuggingFaceBgeEmbeddings(
+            model_name="BAAI/bge-small-en-v1.5", 
+            model_kwargs={'device':'cpu'}, 
+            encode_kwargs={'normalize_embeddings': True}
+        )
+        # Define a subdirectory within the executable's directory to store FAISS data
+        vector_db_dir = os.path.join(script_dir, "vector_db")
 
-    # Create the directory if it doesn't exist
-    os.makedirs(vector_db_dir, exist_ok=True) 
+        # Create the directory if it doesn't exist
+        os.makedirs(vector_db_dir, exist_ok=True) 
 
-    # Load existing vector store
-    vectorstore = FAISS.load_local(vector_db_dir, huggingface_embeddings, allow_dangerous_deserialization=True)
+        # Load existing vector store
+        vectorstore = FAISS.load_local(vector_db_dir, huggingface_embeddings, allow_dangerous_deserialization=True)
 
-    # Add new embeddings
-    await vectorstore.aadd_documents(docs_after_split)
+        # Add new embeddings
+        await vectorstore.aadd_documents(docs_after_split)
 
-    # Save vector database
-    vectorstore.save_local(vector_db_dir)
+        # Save vector database
+        vectorstore.save_local(vector_db_dir)
 
-    return
+        return True
+    
+    except:
+        return False
