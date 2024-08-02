0d86aa7426720e3988534f7267516e83800b9208
adeeshajayasinghe <jayasingheai.20@uom.lk>
2024-08-02 18:26:37 +0530

    Modify the prompt template

--- utils/generate_response.py
+++ utils/generate_response.py
@@ -36,7 +36,7 @@ async def get_llm_response(query):
 
     prompt_template = """Use the following pieces of context to answer the question at the end. Please follow the following rules:
     1. If you don't know the answer, don't try to make up an answer. Just say "I can't find the final answer but you may want to check the following links".
-    2. If you find the answer, write the answer in a concise way as paragraphs.
+    2. Replace any occurrences of numbered or bulleted list items in new lines.
 
     {context}
 
