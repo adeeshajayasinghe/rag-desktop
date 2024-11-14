fb2bc6dee6023e6204153a03260b87d5af5a8d23
adeeshajayasinghe <jayasingheai.20@uom.lk>
2024-11-14 18:35:47 +0530

    Enabled file upload for training

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with st.sidebar:
     )
 
     uploaded_files = st.file_uploader(
-        "Upload your PDFs", type=["pdf"], accept_multiple_files=True, disabled=True
+        "Upload your PDFs", type=["pdf"], accept_multiple_files=True
     )
 
     # Save uploaded PDFs locally
